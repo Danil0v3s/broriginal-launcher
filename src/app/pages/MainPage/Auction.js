e2e01a113c846022f0c955c7cc61e6fa6e033df7
Danilo Lemes <daniloleemes@hotmail.com>
2020-06-18 20:19:00 +0100

    add animation to side panel

--- src/app/pages/MainPage/Auction.js
+++ src/app/pages/MainPage/Auction.js
@@ -34,47 +34,60 @@ export default class Auction extends React.Component {
 
         return this.state.listings.map(entry => {
             return (
-                <div key={entry.auction_id} style={{ display: 'flex', justifyContent: 'space-between', marginBottom: 8 }}>
-                    <img src={`https://www.divine-pride.net/img/items/item/iRO/${entry.nameid}`} />
-                    <span>{entry.item_name}</span>
-                    <span>{entry.price}</span>
-                    <span>{cardInfo(entry)}</span>
-                    <button>Comprar</button>
+                <div key={entry.auction_id} className="auction-list-item" onClick={() => this.setState({ itemSelected: entry })}>
+                    <img src={`https://www.divine-pride.net/img/items/item/iRO/${entry.nameid}`} height={32} style={{ marginLeft: 16 }} />
+                    <span style={{ marginLeft: 16 }}>{entry.item_name}</span>
+                    <span style={{ marginLeft: 'auto', marginRight: 32 }}>+{entry.refine}</span>
+                    <span style={{ marginRight: 32 }}>{entry.price}</span>
+                    <span style={{ marginRight: 32 }}>{cardInfo(entry)}</span>
+                    <button style={{ width: 80 }}>Comprar</button>
                 </div>
             )
         })
     }
 
     render() {
-        const { listings, error } = this.state
+        const { listings, error, itemSelected } = this.state
         return (
             <div className="content-body">
                 <div className="auction">
                     <div className="card" style={{ height: '100%', width: '100%', backgroundColor: 'white' }}>
-                        <div className="card-header" style={{ display: 'flex', justifyContent: 'space-between', width: '100%', flexDirection: 'row' }}>
-                            <div className="" style={{ display: 'flex', flexDirection: 'row', justifyContent: 'space-around' }}>
-                                <Input label="Item ID" secondary style={{ marginRight: 16 }} />
-                                <Input label="Item name" secondary style={{ marginRight: 16 }} />
-                                <Input label="Min. price" secondary style={{ marginRight: 16 }} />
-                                <Input label="Max. price" secondary style={{ marginRight: 16 }} />
+                        <div style={{ display: 'flex', flexDirection: 'row', overflow: 'auto', overflowX: 'hidden' }}>
+                            <div className="auction-filters">
+                                <Input label="Item ID" secondary style={{ marginRight: 16, marginBottom: 8 }} />
+                                <Input label="Item name" secondary style={{ marginRight: 16, marginBottom: 8 }} />
+                                <div style={{ flexDirection: 'row', display: 'flex', marginBottom: 8 }}>
+                                    <Input label="Min. price" secondary style={{ marginRight: 16 }} />
+                                    <Input label="Max. price" secondary style={{ marginRight: 16 }} />
+                                </div>
                             </div>
-                            <img src={icUnfoldMore} />
+                            <div className="auction-listing">
+                                {
+                                    listings.length == 0 && "No items available for sale"
+                                }
+                                {
+                                    listings.length > 0 && (
+                                        <div style={{ width: '100%', display: 'flex', flexDirection: 'column' }}>
+                                            {
+                                                this.renderListingsTable()
+                                            }
+                                        </div>
+                                    )
+                                }
+                            </div>
+                            <div className={`auction-info ${itemSelected ? 'active' : ''}`}>
+                                {
+                                    itemSelected && (
+                                        <>
+                                            <img src={`https://www.divine-pride.net/img/items/collection/iRO/${itemSelected.nameid}`} width={75} style={{ marginLeft: 'auto', marginRight: 'auto' }} />
+
+                                            <button style={{ marginTop: 'auto' }} onClick={() => this.setState({ itemSelected: undefined })}>Fechar</button>
+                                        </>
+                                    )
+                                }
+                            </div>
                         </div>
-                        <div className="auction-listing" style={{ marginTop: 24, marginBottom: 24, overflow: 'auto' }}>
-                            {
-                                listings.length == 0 && "No items available for sale"
-                            }
-                            {
-                                listings.length > 0 && (
-                                    <div style={{ width: '100%', display: 'flex', flexDirection: 'column' }}>
-                                        {
-                                            this.renderListingsTable()
-                                        }
-                                    </div>
-                                )
-                            }
-                        </div>
-                        <div className="card-footer" style={{ display: 'flex', justifyContent: 'space-between', width: '100%', flexDirection: 'row', fontSize: 12, marginTop: 'auto' }}>
+                        <div className="card-footer">
                             <span>{moment().format('MMMM Do YYYY, h:mm:ss a')}</span>
                             <span>Vendas recentes: 0</span>
                             <span>Meus itens à venda: 0</span>
