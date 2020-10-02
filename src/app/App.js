b1c5df11a261c0f6ae3bb4a672b444b48f44c35d
Danilo Lemes <daniloleemes@hotmail.com>
2020-10-02 23:12:11 +0100

    connect app

--- src/app/App.js
+++ src/app/App.js
@@ -6,52 +6,25 @@ import { Router, Switch, Route } from "react-router-dom";
 import history from './components/History';
 import MainPage from './pages/MainPage/MainPage';
 import axios from 'axios';
+import { connect } from 'react-redux';
 
 axios.defaults.baseURL = process.env.NODE_ENV === 'production' ? process.env.API_URL : 'http://localhost:5131/api/';
 axios.defaults.headers.post['Content-Type'] = 'application/json';
 
 const ipcRenderer = window.require('electron').ipcRenderer
 
-export default class App extends React.Component {
+class App extends React.Component {
 
   constructor(props) {
     super(props);
 
-    this.state = {
-      userInfo: { username: 'admin1', password: 'admin1', isAuthenticated: false, account_id: '' }
-    }
-    ipcRenderer.on('asynchronous-message', this.asynchronousMessageFromMain)
+    // ipcRenderer.on('asynchronous-message', this.asynchronousMessageFromMain)
   }
 
-  asynchronousMessageFromMain = async (event, arg) => {
-    if (!this.unmounted && this.mounted) {
-      this.setState({ ...arg })
-    }
-  }
-
-  componentWillUnmount() {
-    this.unmounted = true
-    this.mounted = false
-  }
-
-  componentDidMount() {
-    this.unmounted = false
-    this.mounted = true
-  }
-
-  isLoggedIn() {
-    return this.state.userInfo.isAuthenticated && this.state.userInfo.token !== undefined
-  }
-
-  setUserInfo = (userInfo) => {
-    this.setState({ userInfo });
+  componentDidUpdate() {
+    const { userInfo } = this.props;
     if (userInfo.token) {
       axios.defaults.headers.common['Authorization'] = userInfo.token;
-    }
-    if (this.isLoggedIn()) {
-      history.push('/main/home')
-    } else {
-      history.push('/')
     }
   }
 
@@ -62,15 +35,22 @@ export default class App extends React.Component {
           <TitleBar />
           <Switch>
             <Route path="/main">
-              <MainPage userInfo={this.state.userInfo} />
+              <MainPage />
             </Route>
             <Route path="/">
-              <LoginPage setUserInfo={this.setUserInfo} userInfo={this.state.userInfo} />
+              <LoginPage />
             </Route>
           </Switch>
         </Router>
       </div>
     );
   }
-
 }
+
+const mapStateToProps = ({ auth }) => {
+  return {
+    userInfo: auth.userInfo
+  }
+}
+
+export default connect(mapStateToProps)(App)
